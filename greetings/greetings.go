2a5fcc0cf8f0d495c32775ccc54fddca4e55007e
r0B-O <neo@r0B-O.dev>
2022-02-15 00:25:59 +0530

    Add slice, rand & init func

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,16 +3,38 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 // Function Hello returns a greeting for the named person
 func Hello(name string) (string, error) {
 	// If no name was given, return a message with this error
 	if name == "" {
-		return "", errors.New("Name is empty")
+		return "", errors.New("name is empty")
 	}
 	// Return a message that embeds the name in a message
-	message := fmt.Sprintf("Hi, %v. Welcome", name)
-	
+	// Create a message using a random greeting and format
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// init sets intial values for variables used in the function
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// randomFormat returns one of the set of greeting messages. The returned
+// message is selected at random
+func randomFormat() string {
+	// A slice of message formats.
+	formats := []string{
+		"Hi. %v, Welcome!",
+		"Great to see you, %v",
+		"Hail %v, Well met!",
+	}
+
+	// Return a randomly selected message format by specifying a random
+	// index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
 }
--- track.md
+++ track.md
@@ -10,9 +10,40 @@ log --> Print console logs
 errors --> Generate error messages + ?
     - New --> create new error with a message
 
+math/rand --> Generate random numbers?
+
+time --> Time operations like Now()
+
 ### Tips
 1. Short Declaration
 := can be used to declare and assign a variable
 
 2. nil
 Can be used to validate if a variable is empty?
+
+3. Slices -- Arrays --> Check
+Declaration - []string{
+    "item1",
+    "item2",
+    "item3",
+}
+
+Note: Comma after last item in array
+
+4. init function
+
+init is called after all the variable declarations in the package have evaluated their initializers, and those are evaluated only after all the imported packages have been initialized. 
+
+func init() {
+    if user == "" {
+        log.Fatal("$USER not set")
+    }
+    if home == "" {
+        home = "/home/" + user
+    }
+    if gopath == "" {
+        gopath = home + "/go"
+    }
+    // gopath may be overridden by --gopath flag on command line.
+    flag.StringVar(&gopath, "gopath", gopath, "override default GOPATH")
+}
